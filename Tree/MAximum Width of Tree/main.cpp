8759e16e9febb23440afda13d1057ec53cefdba9
prafull <prafull5tripathi@gmail.com>
2022-03-19 10:43:32 +0530

    Commit
    2 solution of Maximum Width of Tree

---
+++ Tree/MAximum Width of Tree/main.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *l;
+    struct node *r;
+} *root=NULL, *nn;
+
+struct node * create(int);
+struct node * insert_data();
+void levelOrder(node *);
+int maxWidth(node *);
+
+int main()
+{
+    int op;
+    do
+    {
+        cout<<"\nMenu\n 0. Exit\n";
+        cout<<" 1. Insert in binary tree\n 2. Level order Traversal\n 3. Maximum width of Tree\n";
+        cin>>op;
+        switch(op)
+        {
+            case 0: exit(0);break;
+            case 1: system("cls");insert_data();break;
+            case 2: system("cls");
+                    {
+                        cout<<"Level order Traversal"<<"\n";
+                        levelOrder(root);
+                    }
+                    break;
+            case 3: system("cls");
+                    {
+                        cout<<"Maximum width of Tree is:\n"<<maxWidth(root)<<"\n";
+                    }
+                    break;
+        }
+    }while(op!=4);
+    return 0;
+}
+
+struct node * create(int val){
+    nn=new node();
+    if(!nn)
+    {
+        cout << "Memory error\n";
+        return NULL;
+    }
+    nn->data=val;
+    nn->l = nn->r = NULL;
+    return nn;
+};
+
+struct node * insert_data()
+{
+    int val;
+    cout<<"Value to be entered: (-1) for null\n";
+    cin>>val;
+    if(root==NULL)
+    {
+        root=create(val);
+        return root;
+    }
+    queue< node *>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node *temp=q.front();
+        q.pop();
+        if(temp->l!=NULL)
+        {
+            if(temp->l->data!=-1)
+                q.push(temp->l);
+        }
+
+        else
+        {
+            if(val==-1)
+            {
+                temp->l=create(val);
+                return root;
+            }
+            temp->l=create(val);
+            return root;
+        }
+
+        if(temp->r!=NULL)
+        {
+            if(temp->r->data!=-1)
+                q.push(temp->r);
+        }
+
+        else
+        {
+            if(val==-1)
+            {
+                temp->r=create(val);
+                return root;
+            }
+            temp->r=create(val);
+            return root;
+        }
+    }
+    return 0;
+};
+
+void levelOrder(node * root)
+{
+    if(root== NULL)
+    {
+        cout<<"Empty Tree\n";
+        return;
+    }
+    queue< node *>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        node *temp=q.front();
+        cout<<temp->data<<" ";
+        q.pop();
+        if(temp->l!=NULL && temp->l->data!=-1)
+            q.push(temp->l);
+
+        if(temp->r!=NULL && temp->r->data!=-1)
+            q.push(temp->r);
+
+    }
+}
+
+int maxWidth(node * root)
+{
+    if(root==NULL)
+        return 0;
+
+    int result=0;
+    queue<node *>q;
+    q.push(root);
+    while(!q.empty())
+    {
+        int count=q.size();
+        result=max(result,count);
+
+        while(count--)
+        {
+            node *temp=q.front();
+            q.pop();
+
+            if(temp->l)
+                q.push(temp->l);
+
+            if(temp->r)
+                q.push(temp->r);
+        }
+    }
+    return result;
+}
